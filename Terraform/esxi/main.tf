30bd04e70272bdce16762111975b5b40d8d29fb7
Julian Maze <jmmaze@outlook.com>
2025-11-16 22:11:05 -0800

    feat(terraform,esxi): k3s plex node

--- Terraform/esxi/main.tf
+++ Terraform/esxi/main.tf
@@ -8,6 +8,14 @@ resource "esxi_virtual_disk" "longhorn" {
   virtual_disk_type       = "thin"
 }
 
+resource "esxi_virtual_disk" "longhorn_plex" {
+  virtual_disk_disk_store = var.disk_store
+  virtual_disk_dir        = "k3s-cilium-plex"
+  virtual_disk_name       = "longhorn.vmdk"
+  virtual_disk_size       = 200
+  virtual_disk_type       = "thin"
+}
+
 resource "esxi_guest" "k3s-cilium" {
   count                  = var.cilium_node_count
   guest_name             = "k3s-cilium-${count.index + 1}"
@@ -64,6 +72,39 @@ resource "esxi_guest" "k3s-cilium-vpn" {
   }
 
   power = "off"
+
+  lifecycle {
+    ignore_changes = [power]
+  }
+}
+
+resource "esxi_guest" "k3s-cilium-plex" {
+  guest_name             = "k3s-cilium-plex"
+  guest_shutdown_timeout = 20
+  guest_startup_timeout  = 120
+
+
+  boot_firmware  = "efi"
+  boot_disk_size = "100" # 100 GB
+  boot_disk_type = "thin"
+  disk_store     = var.disk_store
+  memsize        = "16384" # 16 GB
+  numvcpus       = "4"
+  guestos        = "ubuntu-64"
+
+  network_interfaces {
+    virtual_network = "Docker VLAN 25"
+    nic_type        = "vmxnet3"
+  }
+
+  power = "off"
+
+  # Additional Disks
+  # - Longhorn
+  virtual_disks {
+    virtual_disk_id = esxi_virtual_disk.longhorn_plex.id
+    slot            = "0:1"
+  }
 
   lifecycle {
     ignore_changes = [power, virtual_disks[0].virtual_disk_id]
